bcc0b946066654c5655a15c1c3aaa68d906ba484
alice958 <81510055+alice958@users.noreply.github.com>
2021-05-08 00:03:31 +0530

    Added: To  check the random no

---
+++ dowhile/RandomNo.js
@@ -0,0 +1,7 @@
+/* @Description - to check heads or tail using random keyword .*/
+
+let random = Math.floor(Math.random() * 10) % 2;
+if (random == 0)
+    console.log("It’s Head");
+else
+    console.log("It's Tail");
